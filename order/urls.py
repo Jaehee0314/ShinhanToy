437ad81c2a9578c5509139c1f3511b19ed6e8d50
Jaehee0314 <ran_ed@naver.com>
2023-01-27 15:57:03 +0900

    feat: pagination

--- order/urls.py
+++ order/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("/<int:pk>", views.OrderDetailView.as_view()),
     path("/<int:order_id>/comment", views.CommentListView.as_view()),
     path("/<int:order_id>/comment/write", views.CommentCreateView.as_view()),
+   
   
 
 ]
